a484a5641023b5167469cc663c03b09b134ca730
David-Rodden <davidrodden273@gmail.com>
2019-01-23 11:48:05 -0800

    Now selects to run at dynamic intervals

--- src/tasks/WalkToSpecified.java
+++ src/tasks/WalkToSpecified.java
@@ -1,16 +1,20 @@
 package tasks;
 
 import org.rspeer.runetek.api.commons.Time;
+import org.rspeer.runetek.api.commons.math.Random;
 import org.rspeer.runetek.api.movement.Movement;
 import org.rspeer.runetek.api.movement.position.Position;
 import task_structure.TreeTask;
 
 public class WalkToSpecified extends TreeTask {
+    private static final int MAXIMUM_DESTINATION_DISTANCE = 5;
     private final Position destination;
+    private int runThreshold;
 
     WalkToSpecified(final Position destination) {
         super(true);
         this.destination = destination;
+        runThreshold = Random.high(15, 30);
     }
 
     @Override
@@ -20,16 +24,14 @@ public class WalkToSpecified extends TreeTask {
 
     @Override
     public int execute() {
-        if (destination == null) {
-            System.out.println("destination is null");
-            return super.execute();
-        }
-        if (!Movement.isRunEnabled() && Movement.getRunEnergy() > 20) {
+        if (destination == null) return super.execute();
+        if (!Movement.isRunEnabled() && Movement.getRunEnergy() > runThreshold) {
             Movement.toggleRun(true);
             Time.sleepUntil(Movement::isRunEnabled, 1000);
+            runThreshold = Random.high(15, 30);
         }
         Movement.walkTo(destination);
-        Time.sleepUntil(() -> !Movement.isDestinationSet() || Movement.getDestinationDistance() < 5, 5000);
+        Time.sleepUntil(() -> !Movement.isDestinationSet() || Movement.getDestinationDistance() < MAXIMUM_DESTINATION_DISTANCE, 5000);
         return super.execute();
     }
 }
